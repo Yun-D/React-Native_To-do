d70d49c851be0c72a6518180dc8a33c7f88410f9
Yun-D <yund_272@naver.com>
2020-09-21 17:53:40 +0900

    아이콘 세팅 완료, todo 만들기 시작

--- App.js
+++ App.js
@@ -4,6 +4,10 @@ import {SafeAreaView, StyleSheet, Text} from 'react-native';
 import {createAppContainer} from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
 import 'react-native-gesture-handler';
+
+//icon 불러오기
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 //컴포넌트 스크린 가져오기
 import HomeScreen from './HomeScreen';
 import TodoScreen from './TodoScreen';
@@ -15,6 +19,29 @@ const TabNavigator = createBottomTabNavigator({
   Todo: {
     screen: TodoScreen,
   },
-});
+}, 
+{
+  defaultNavigationOptions: ({navigation}) => ({
+    tabBarIcon: ({horizontal, tintColor}) => {
+      const {routeName} = navigation.state;
+      let iconName;
+
+      if(routeName === 'Home') {
+        iconName = 'home';
+      } else if (routeName === 'Todo') {
+        iconName = 'flag-sharp';
+      }
+
+      return (
+        <Ionicons 
+          name = {iconName}
+          size = {horizontal ? 20 : 25}
+          color = {tintColor}
+        />
+      );
+    },
+  }),
+  },
+);
 
 export default createAppContainer(TabNavigator);
