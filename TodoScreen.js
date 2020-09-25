9345fd15363309ca7911b6824652a1553b42af17
Yun-D <yund_272@naver.com>
2020-09-26 01:17:34 +0900

    [200926] 카드뷰 추가

--- TodoScreen.js
+++ TodoScreen.js
@@ -5,11 +5,12 @@ import { TextInput } from 'react-native-paper';
 class TodoScreen extends React.Component {
   render() {
     return (
-      //<Text style = {styles.appTitle}>Todo!</Text>
-      <View style = {styles.card}>
-        <TextInput style={styles.input} placeholder = "Add an item.." />
-      </View>
-
+      <View style={styles.container}>
+        <Text style={styles.appTitle}>Todo!</Text>
+        <View style={styles.card}>
+          <TextInput style={styles.input} placeholder="Add an item!" />
+        </View>
+    </View>
     );
   }
 }
@@ -27,6 +28,22 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textAlign: 'center',
     //backgroundColor: '#3143e8',
+  },
+  card: {
+    backgroundColor: '#fff',
+    flex: 1,
+    borderTopLeftRadius: 10, // to provide rounded corners
+    borderTopRightRadius: 10, // to provide rounded corners
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  input: { //아이템 추가
+    backgroundColor: '#fff',
+    //padding: 10,
+    borderBottomColor: '#bbb',
+    borderBottomWidth: 1,
+    fontSize: 20,
+    marginLeft: 20,
   },
 });
 
